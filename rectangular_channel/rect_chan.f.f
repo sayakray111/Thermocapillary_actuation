e779c893a8a5f30607b686da2e56ee35eb586163
Sayak Ray <sayokroy56@gmail.com>
2021-08-23 09:50:25 +0530

    rectangular_channel/

---
+++ rectangular_channel/rect_chan.f.f
@@ -0,0 +1,14 @@
+c============================
+c Author: S.Ray
+c Micorfluidics Lab, IITKGP 2020
+c============================
+
+c-------------------------------------
+c Green's function of Laplace equation
+c------------------------------------- 
+
+Implicit Double Precision (a-h,o-z)
+Dimension NSG(100),NE(100),Ncol(ncol)
+Dimension xw(100,100),yw(100,100),tw(100,100)
+Dimension WWW(3)
+Dimension 
