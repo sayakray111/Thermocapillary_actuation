bbd43cc55bb8a7df893237d51595ec4bc9975bda
Sayak Ray <sayokroy56@gmail.com>
2021-08-26 12:23:46 +0530

    rectangular_channel/

--- rectangular_channel/lines_arc_break.f
+++ rectangular_channel/lines_arc_break.f
@@ -10,8 +10,8 @@ c============================
           Dimension ptsx(500),ptsy(500)
 		
           error_margin = 0.0001D0
-		nlinepts = 0
-		ncurvepts = 0
+		  nlinepts = 0
+		  ncurvepts = 0
           common xxx01/ ptsx,ptsy
           common xxx02/ line_points,curve_points
 
@@ -19,19 +19,19 @@ c============================
 c---------------------------------------
 c Filling the matrices with zero values
 c------------------------------------------
-		do ll = 1,500
+		  do ll = 1,500
 			line_points(ll,:) = 0
-		end do
-		do ll = 1,500
+		  end do
+		  do ll = 1,500
 			curve_points(ll,:) = 0
-		end do
+		  end do
 c------------------------------------------------
 c Extracting the collinear points from the set
 c-------------------------------------------------
-		inc = 1
-		p1 = 0
-		pl = 1
-		flag = 0
+		  inc = 1
+		  p1 = 0
+		  pl = 1
+		  flag = 0
           do i=1,len(ptsx),inc
 		     slope1=(ptsy(i+1)-ptsy(i))/(ptsx(i+1)-ptsx(i))
 		     do k = 2,len(ptsx)-i
@@ -53,30 +53,32 @@ c-------------------------------------------------
 		     end do
 		     inc = k-1
 		     flag = 0
-		end do
+          end do
 c----------------------------------------------------------------------------
 c find out the number of elements stored in the arrays ----------------------
 c---------------------------------------------------------------------------
-		do jk = 1,len(line_points)
+		  do jk = 1,len(line_points)
 			if(line_points(jk,1).neq.0)
 			  nlinepts+=1
-	end do
+			end if
+	      end do
 	
-	do jk = 1,len(curve_points)
+	      do jk = 1,len(curve_points)
 			if(curve_points(jk,1).neq.0)
 			  ncurvepts+=1
-		end do
+			end if
+		  end do
 c-----------------------------------------------------------------------------------------------
 c Cleaning the curved sections data and assigning zero elements and last element to the curved set
 c---------------------------------------------------------------------------------------------		
-		do h = 1,len(line_points)
+		  do h = 1,len(line_points)
               do a = 1,len(curve_points)
                   if(line_points(h,1).eq.curve_points(a,1))then
                       curve_points(a,1) = 0
                       curve_points(a,2) = 0
-				end if
-			end do
-		end do
+				  end if
+			  end do
+		  end do
 	    
-		return 
+		  return 
           end
